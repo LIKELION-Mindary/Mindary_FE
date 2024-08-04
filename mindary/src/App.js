7ca0583291d0874d3451dd998af995d179295f31
chaeyoungwon <epopcy@naver.com>
2024-08-04 14:37:48 +0900

    Feat: 랜딩페이지 UI 구현

--- mindary/src/App.js
+++ mindary/src/App.js
@@ -4,14 +4,16 @@ import Home from "./pages/Home";
 import Record from "./pages/Record";
 import KakaoLogin from "./components/Auth/KakaoLogin";
 import Archieve from "./pages/Archieve";
-
 import { ThemeProvider } from "./styles/ThemeContext";
+import Landing from "./pages/Landing";
+
 const App = () => {
   return (
     <BrowserRouter>
       <ThemeProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Home />} />
           <Route path="/accounts/kakao" element={<KakaoLogin />} />
           <Route path="/mindary" element={<Record />} />
           <Route path="/mindary/records/archieve" element={<Archieve />} />
